84aaba37187c5fd14c2b340c2224dd8d32cac688
adriana.del.teso <adriana.del.teso@accenture.com>
2021-11-02 16:36:22 +0100

    :sparkles: Add component 'items-details'

---
+++ src/app/item-details/item-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-item-details',
+  templateUrl: './item-details.component.html',
+  styleUrls: ['./item-details.component.scss']
+})
+export class ItemDetailsComponent implements OnInit {
+  @Output() deleteRequest = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  delete() {
+    this.deleteRequest.emit('item name');
+  }
+
+}
