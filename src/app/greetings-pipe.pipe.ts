489cd208c8a63e5bba9d17203f51116fb5a3caa1
adriana.del.teso <adriana.del.teso@accenture.com>
2021-11-02 12:02:03 +0100

    :art: Add greetings pipe

--- src/app/greetings-pipe.pipe.ts
+++ src/app/greetings-pipe.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+// Pipe decorator, that is used to call the pipe in our templates
 @Pipe({
   name: 'greetingsPipe'
 })
+  
+  // The PipeTransform interface need to be implemented.
+  // This interface requires the transform method to be implemented
 export class GreetingsPipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  // This method receives a value passed from a template. In this case it will be a string, 
+  // and the second parameter is for optional params
+  transform(value: unknown, ...args: unknown[]): string {
+    return `Hello, ${value}`;
   }
 
 }
